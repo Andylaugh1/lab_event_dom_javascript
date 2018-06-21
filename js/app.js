a5552662c11eb08870f8b1a425b863004e52e362
Andy Laughlin <atl11384@gmail.com>
2018-06-21 16:30:04 +0100

    Final commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+
+  const handleFormSubmit = function (event) {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const title = form.title.value;
+    const author = form.author.value;
+    const category = form.category.value;
+    createBook(title, author, category);
+
+    }
+
+    const createBook = function(bookTitle, bookAuthor, bookCategory) {
+      const book = document.createElement();
+      const container = document.querySelector('#reading-list')
+      container.appendChild(book);
+  }
+
+
+  form.addEventListener('submit', handleFormSubmit);
+
+
+
 })
